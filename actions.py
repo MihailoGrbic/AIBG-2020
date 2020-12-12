86855e6dfc6567011d7b1be3dfba46759d646333
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 01:26:37 +0100

    Fixed actions

--- actions.py
+++ actions.py
@@ -9,8 +9,8 @@ def bail_out(): return "bailOut"
 def dig(): return "dig"
 def bury(): return "bury"
 def collect(): return "d"
-def drop_part(part_id): return "dropPart-" + part_id
-def swap_part(part_id): return "swapPart-" + part_id
+def drop_part(part_id): return f"dropPart-{part_id}"
+def swap_part(part_id): return f"swapPart-{part_id}"
 def place_trap(): return "trap"
 
 
@@ -18,9 +18,9 @@ def place_trap(): return "trap"
 def buy_potion(): return "buy-potion"
 def use_potion(): return "usePotion"
 def buy_trap(): return "buy-trap"
-def buy_part(part_id): return "buy-" + part_id
-def sell_part(part_id): return "sell-" + part_id
-def trade_parts(give_part_id, take_part_id): return "trade-" + give_part_id + "-" + take_part_id
-def sell_part_with_curse(part_id): return "sellPartWithCurse-" + part_id
-def trade_parts_with_curse(give_part_id, take_part_id): return "tradeWithCurse-" + give_part_id + "-" + take_part_id
+def buy_part(part_id): return f"buy-{part_id}"
+def sell_part(part_id): return f"sell-{part_id}"
+def trade_parts(give_part_id, take_part_id): return f"trade-{give_part_id}-{take_part_id}"
+def sell_part_with_curse(part_id): return f"sellPartWithCurse-{part_id}"
+def trade_parts_with_curse(give_part_id, take_part_id): return f"tradeWithCurse-{give_part_id}-{take_part_id}"
 def sell_totem(): return "sellTotem"
