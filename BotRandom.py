205606e5474125c971de216338bf7814a4600907
Mihailo_Grbic <mihailogrbic99@gmail.com>
2020-12-12 15:34:35 +0100

    Minor Improvements to BotCompetitiveBodyBlock

--- BotRandom.py
+++ BotRandom.py
@@ -1,6 +1,7 @@
 from Bot import Bot
 from GameState import GameState
 import actions
+from utils import move_available
 import random
 
 class BotRandom(Bot):
@@ -8,5 +9,13 @@ class BotRandom(Bot):
         self.counter = 0
 
     def play_single_turn(self, current_game_state):
-        action_list = [actions.left(), actions.right(), actions.up(), actions.down()]
-        return action_list[random.randrange(4)]
+        action_list = []
+        if move_available(current_game_state.map, current_game_state.other_info, (current_game_state.x + 1, current_game_state.y)):
+            action_list.append(actions.right())
+        if move_available(current_game_state.map, current_game_state.other_info, (current_game_state.x - 1, current_game_state.y)):
+            action_list.append(actions.left())
+        if move_available(current_game_state.map, current_game_state.other_info, (current_game_state.x, current_game_state.y + 1)):
+            action_list.append(actions.down())
+        if move_available(current_game_state.map, current_game_state.other_info, (current_game_state.x, current_game_state.y - 1)):
+            action_list.append(actions.up())
+        return action_list[random.randrange(len(action_list))]
