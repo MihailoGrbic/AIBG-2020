c2158a5a8bae87c53ae5b7858296ee900d97d993
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 02:30:54 +0100

    Added Tile class

--- Map.py
+++ Map.py
@@ -1,10 +1,38 @@
+class Tile:
+
+    def __init__(self, tile: dict):
+        self.tile = tile
+        self.x = tile.get('x', -1)
+        self.y = tile.get('y', -1)
+        self.blocked = tile.get('x', False)
+
+    def __getitem__(self, item):
+        return self.tile[item]
+
+    def __setitem__(self, key, value):
+        self.tile[key] = value
+        if key == "x":
+            self.x = value
+        if key == "y":
+            self.y = value
+
+
+def create_tile_map(res_tiles):
+    tiles = []
+    for row in res_tiles:
+        tiles.append([])
+        for tile in row:
+            tiles[-1].append(Tile(tile))
+    return tiles
+
+
 class Map(object):
     def __init__(self, res):
         res = res['map']
         self.size = 25
         self.width = res['width']
         self.height = res['height']
-        self.tiles = res['tiles']
+        self.tiles = create_tile_map(res['tiles'])
 
         self.items = []
 
@@ -12,3 +40,6 @@ class Map(object):
         r_x = self.width - x - 1
         r_y = self.height - y - 1
         return r_x, r_y
+
+    def get_tile(self, x, y):
+        return self.tiles[y][x]
