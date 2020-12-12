41e7cf6461ce5cfb2f4ea97bfbd18429592d9ba5
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 07:06:06 +0100

    Added dvorak to keyboard controller

--- BotKeyboard.py
+++ BotKeyboard.py
@@ -3,6 +3,18 @@ from Bot import Bot
 
 class BotKeyboard(Bot):
 
+    def __init__(self, dvorak: bool = False):
+        self.dvorak = dvorak
+
     def play_single_turn(self, current_game_state):
         command = input("Your move, bitch: ")
+        if self.dvorak:
+            if command == ',':
+                return 'w'
+            if command == 'a':
+                return 'a'
+            if command == 'o':
+                return 's'
+            if command == 'e':
+                return 'd'
         return command
