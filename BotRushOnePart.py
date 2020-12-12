97ee4f79bbf2126b52830320086116e2238dd709
Mihailo_Grbic <mihailogrbic99@gmail.com>
2020-12-12 06:23:21 +0100

    Minor changes

--- BotRushOnePart.py
+++ BotRushOnePart.py
@@ -3,7 +3,7 @@ from utils import *
 
 class BotRushOnePart(Bot):
 
-    def __init__(self):
+    def play_single_turn(self, current_game_state):
         start_x = current_game_state.self_info.x
         start_y = current_game_state.self_info.y
         if start_x > 12: self.target_x = 13
@@ -11,10 +11,9 @@ class BotRushOnePart(Bot):
         if start_y > 12: self.target_y = 13
         else:  self.target_y = 11
 
-    def play_single_turn(self, current_game_state):
         print(current_game_state.turns_left)
         for tile in current_game_state.map.tiles:
-            if tile['tileType'] == 'DIGTILE':
+            if tile.__getitem__('tileType') == 'DIGTILE':
                 print("vuhu")
         return move_once(current_game_state, target=(self.target_x, self.target_y))
         
