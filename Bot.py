049e3d42987ecd7c2fa6b61cc48666358d84423a
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 06:07:02 +0100

    Added current_game_state to Policy list

--- Bot.py
+++ Bot.py
@@ -12,11 +12,11 @@ class Bot(object):
     def play_single_turn(self, current_game_state: GameState):
         pass
 
-    def get_policy_list(self):
+    def get_policy_list(self, current_game_state: GameState = None):
         return list()
 
     def resolve_policy(self, current_game_state: GameState) -> Bot:
-        for policy in self.get_policy_list():
+        for policy in self.get_policy_list(current_game_state):
             if policy.should_execute(current_game_state):
                 return policy.bot
         return None
