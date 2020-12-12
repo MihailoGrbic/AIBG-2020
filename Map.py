e86222bd9c2b5040cdc6a7cb90746577e4349a01
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 06:14:25 +0100

    Removed Tile class

--- Map.py
+++ Map.py
@@ -1,54 +1,10 @@
-class Tile:
-
-    def __init__(self, tile: dict):
-        self.tile = tile
-        self.x = tile.get('x', -1)
-        self.y = tile.get('y', -1)
-        self.blocked = tile.get('x', False)
-        self.discovered = False
-
-    def __getitem__(self, item: str):
-        return self.tile[item]
-
-    def __setitem__(self, key, value):
-        self.tile[key] = value
-        if key == "x":
-            self.x = value
-        if key == "y":
-            self.y = value
-        if key == "BLOCKTILE":
-            self.blocked = True
-        self.discovered = True
-
-    def __contains__(self, item):
-        return item in self.tile
-
-    def __len__(self):
-        return len(self.tile)
-
-    def __bool__(self):
-        return bool(self.tile)
-
-    def get(self, k, default=None):
-        return self.tile.get(k)
-
-
-def create_tile_map(res_tiles) -> list:
-    tiles = []
-    for row in res_tiles:
-        tiles.append([])
-        for tile in row:
-            tiles[-1].append(Tile(tile))
-    return tiles
-
-
 class Map(object):
     def __init__(self, res):
         res = res['map']
         self.size = 25
         self.width = res['width']
         self.height = res['height']
-        self.tiles = create_tile_map(res['tiles'])
+        self.tiles = res['tiles']
 
         self.tiles[11][11] = {'x': 11, 'y': 11, 'tileType': 'BLOCKTILE', 'shop': True}
         self.tiles[12][11] = {'x': 11, 'y': 12, 'tileType': 'BLOCKTILE', 'shop': True}
