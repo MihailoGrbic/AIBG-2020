4c9429a8af7100ad1484039aa913c00eca760b46
djokjula <djordjemarjanovic998@gmail.com>
2020-12-12 01:40:09 +0100

    Actually adding the code I changed... LOL

--- BotExplore.py
+++ BotExplore.py
@@ -1,13 +1,20 @@
 from Bot import Bot
 from GameState import GameState
-from utils import get_all_undiscovered_tiles, find_closest_coordinate, move_once, random_movement_action
+from utils import get_all_undiscovered_tiles, find_closest_coordinate, move_once, random_movement_action, \
+    get_discovery_tiles_per_direction
 from BotGoTo import BotGoTo
 from Policy import PolicyAlwaysAllow
+from actions import move_actions
 
 
 class BotExplore(Bot):
 
     def play_single_turn(self, current_game_state: GameState):
+        discoveries = get_discovery_tiles_per_direction(current_game_state.map, current_game_state.self_info.pos)
+        best_action = min(move_actions, key=lambda direction: discoveries[direction])
+        if discoveries[best_action] != 0:
+            return best_action
+
         possible_targets = get_all_undiscovered_tiles(current_game_state.map)
         if len(possible_targets) == 0:
             print("The whole world is explored, I'll just wonder aimlessly...")
